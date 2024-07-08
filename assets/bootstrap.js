7c68bcaaff483ff11e592b33efbbd6800e04734d
Rasenti <quentinorias@gmail.com>
2024-07-08 12:42:20 +0200

    Config init

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,13 @@
-import { startStimulusApp } from '@symfony/stimulus-bundle';
+// assets/bootstrap.js
+import './styles/app.css';
+import './bootstrap';
 
-const app = startStimulusApp();
-// register any custom, 3rd party controllers here
-// app.register('some_controller_name', SomeImportedController);
+// If you're using Stimulus, uncomment the following lines
+// import { startStimulusApp } from '@symfony/stimulus-bridge';
+// import '@symfony/autoimport';
+
+// startStimulusApp(require.context(
+//     '@symfony/stimulus-bridge/lazy-controller-loader!./controllers',
+//     true,
+//     /\.(j|t)sx?$
+// ));
