0e61b866a7564c595fbdf0384b600078dd0f659c
Rasenti <quentinorias@gmail.com>
2024-09-02 10:04:45 +0200

    Nav finished

--- assets/app.js
+++ assets/app.js
@@ -8,24 +8,27 @@ import './bootstrap';
 import './styles/app.css';
 console.log('app.js correctement importé')
 
-// Script pour gérer l'ouverture et la fermeture de la modale de login
+// Login modal open/close script
 document.addEventListener('DOMContentLoaded', (event) => {
     const modal = document.getElementById('loginModal');
     const btn = document.getElementById('loginBtn');
     const closeBtn = document.getElementsByClassName('close')[0];
+    const backdrop = document.getElementById('modalBackdrop');
 
+    // Open modal on click on connection
     btn.onclick = function() {
         modal.classList.remove('hidden');
         console.log('click on modal')
     }
 
+    // Close modal on click on close button
     closeBtn.onclick = function() {
         modal.classList.add('hidden');
     }
 
-    window.onclick = function(event) {
-        if (event.target === modal) {
-            modal.classList.add('hidden');
-        }
+    // Close modal on click outside of it
+    backdrop.onclick = function() {
+        modal.classList.add('hidden');
+        console.log('Modal closed by clicking outside');
     }
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
     extend: {
       colors: {
         'theme-dark': '#2d2d2d',
+        'theme-darker': '#232323',
         'theme-light': '#f6f6f6',
         'theme-aqua': '#2dedb9',
         'theme-aqua-darker': '#26cfa2',
