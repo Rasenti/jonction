dd05eb9d1039e01c7ba01c05e42de916cec9e422
Rasenti <quentinorias@gmail.com>
2024-09-23 23:16:35 +0200

    Login fix with modale

--- assets/js/components/connection-modal.js
+++ assets/js/components/connection-modal.js
@@ -18,4 +18,41 @@ export function initModal() {
     backdrop.onclick = function() {
         modal.classList.add('hidden');
     }
+
+    document.addEventListener('DOMContentLoaded', function () {
+        let loginModal = document.getElementById('loginModal');
+
+        if (loginModal) {
+            let form = loginModal.querySelector('form');
+
+            form.addEventListener('submit', function (event) {
+                event.preventDefault(); // Empêche la soumission normale du formulaire
+
+                let formData = new FormData(form);
+                let xhr = new XMLHttpRequest();
+                xhr.open('POST', form.action, true);
+
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState === XMLHttpRequest.DONE) {
+                        if (xhr.status === 200) {
+                            // Parse the response to extract the modal body content
+                            let parser = new DOMParser();
+                            let responseDoc = parser.parseFromString(xhr.responseText, 'text/html');
+                            let newModalBody = responseDoc.querySelector('.modal-body').innerHTML;
+
+                            // Replace the modal body content with the new content
+                            let modalBody = form.querySelector('.modal-body');
+                            modalBody.innerHTML = newModalBody;
+                        } else {
+                            // Handle errors
+                            form.querySelector('.modal-body').innerHTML = '<div class="alert alert-danger">Une erreur s\'est produite. Veuillez réessayer plus tard.</div>';
+                        }
+                    }
+                };
+
+                xhr.send(formData);
+            });
+        }
+    });
+
 }
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -10,7 +12,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils) : Response
+    public function login(Request $request, AuthenticationUtils $authenticationUtils) : Response
     {
         if ($this->getUser()) {
             return $this->redirectToRoute('app_home');
@@ -21,12 +23,25 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        if ($request->isXmlHttpRequest()) {
+            return $this->render('security/login.html.twig', [
+                'last_username' => $lastUsername,
+                'error' => $error,
+            ]);
+        }
+
+        return $this->render('home/home.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error,
+        ]);
     }
 
+    /**
+     * @throws Exception
+     */
     #[Route(path: '/logout', name: 'app_logout')]
     public function logout(): Response
     {
-        return $this->redirectToRoute('app_home');
+        throw new Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
     }
 }
